deb19f9fe48b7a8e54c38998effd25604578325b
vincentc658 <vincentc658@gmail.com>
2020-10-02 21:43:41 +0700

    Third Commit

--- app/src/main/java/com/example/transportation/frameworktesting/MainActivity.kt
+++ app/src/main/java/com/example/transportation/frameworktesting/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.transportation.frameworktesting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.transportation.testlibrary.TestingLibraryActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvTest.setOnClickListener {
+            val intent = Intent(this,TestingLibraryActivity::class.java )
+            startActivity(intent)
+        }
     }
 }
---
+++ testlibrary/src/main/java/com/example/transportation/testlibrary/TestingLibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.transportation.testlibrary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TestingLibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.testing_library_activity)
+    }
+}
